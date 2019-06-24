d2ebb5d8cd4c91f8e436bf150b6957982a2cec7a
Yousef Shanawany <askyous@gmail.com>
2019-06-23 17:17:01 -0700

    Removed duplicates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -24,8 +24,9 @@ export const dialogflowFulfillment = functions.https.onRequest((request, respons
         const query = agent.parameters.person as string;
         const person = await idb.queryForPerson(query);
         const teachers = (await idb.getTeachersAndStudentsOf(person.id)).filter(x => x.student.id === person.id);
+        const uniqueteachers = Array.from(new Set(teachers.map(t => t.teacher.name)));
         if (teachers.length)
-            agent.add(`${person.name}'s teachers include: ${teachers.map(t => t.teacher.name).join(", ")}`);
+            agent.add(`${person.name}'s teachers include: ${uniqueteachers.join(", ")}`);
         else
             agent.add(`Sorry. There is no currently information on the teachers of ${person.name}.`);
     });
@@ -34,8 +35,9 @@ export const dialogflowFulfillment = functions.https.onRequest((request, respons
         const query = agent.parameters.person as string;
         const person = await idb.queryForPerson(query);
         const students = (await idb.getTeachersAndStudentsOf(person.id)).filter(x => x.teacher.id === person.id);
+        const uniquestudents = Array.from(new Set(students.map(t => t.student.name)));
         if (students.length)
-            agent.add(`${person.name}'s learned from: ${students.map(t => t.student.name).join(", ")}`);
+            agent.add(`${person.name}'s learned from: ${uniquestudents.join(", ")}`);
         else
             agent.add(`Sorry. There is no currently information on the students of ${person.name}.`);
     });
@@ -44,8 +46,9 @@ export const dialogflowFulfillment = functions.https.onRequest((request, respons
         const query = agent.parameters.person as string;
         const person = await idb.queryForPerson(query);
         const praisers = (await idb.getPraisersAndPraisesFor(person.id)).filter(x => x.praisee.id === person.id);
+        const uniquepraisers = Array.from(new Set(praisers.map(t => t.praiser.name)));
         if (praisers.length)
-            agent.add(`${person.name} was praised by: ${praisers.map(t => t.praiser.name).join(", ")}`);
+            agent.add(`${person.name} was praised by: ${uniquepraisers.join(", ")}`);
         else
             agent.add(`Sorry. There is no currently information on who praised of ${person.name}.`);
     });
@@ -54,8 +57,9 @@ export const dialogflowFulfillment = functions.https.onRequest((request, respons
         const query = agent.parameters.person as string;
         const person = await idb.queryForPerson(query);
         const praisees = (await idb.getPraisersAndPraisesFor(person.id)).filter(x => x.praiser.id === person.id);
+        const uniquepraisees = Array.from(new Set(praisees.map(t => t.praisee.name)));
         if (praisees.length)
-            agent.add(`${person.name} praised: ${praisees.map(t => t.praisee.name).join(", ")}`);
+            agent.add(`${person.name} praised: ${uniquepraisees.join(", ")}`);
         else
             agent.add(`Sorry. There is no currently information who ${person.name} praised.`);
     });
